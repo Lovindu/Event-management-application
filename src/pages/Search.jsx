4b58a26f7d15ab2b7f74825f2469c9772190eb04
Tharindudaminthaovt <ovtdamintha@gmail.com>
2024-03-23 17:48:22 +0530

    UI updated

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,136 @@
 import React from 'react'
+import { useState } from 'react';
+import './Search.css';
 import Nav from '../components/Nav'
+import EventCard from '../components/EventCard.jsx';
+import Footer from '../components/Footer.jsx';
 
 const Search = () => {
+  const [cardData, setCardData] = useState([{
+    date: "29",
+    month: "Feb",
+    rating: "4.8",
+    img: "../assets/cover.png",
+    Title: "example event",
+    location: "colombo",
+    category: "musical"
+  },
+  {
+    date: "29",
+    month: "Feb",
+    rating: "4.8",
+    img: "../assets/cover.png",
+    Title: "example event",
+    location: "colombo",
+    category: "musical"
+  },
+  {
+    date: "29",
+    month: "Feb",
+    rating: "4.8",
+    img: "../assets/cover.png",
+    Title: "example event",
+    location: "colombo",
+    category: "musical"
+  },
+  {
+    date: "29",
+    month: "Feb",
+    rating: "4.8",
+    img: "../assets/cover.png",
+    Title: "example event",
+    location: "colombo",
+    category: "musical"
+  },
+  {
+    date: "29",
+    month: "Feb",
+    rating: "4.8",
+    img: "../assets/cover.png",
+    Title: "example event",
+    location: "colombo",
+    category: "musical"
+  },]);
   return (
     <div>
-      <Nav />
-      Search
+      <div>
+        <Nav />
+
+        <div className="container">
+          <div className="filters-left">
+            <div className="date-filter">
+              <h2>Date</h2>
+
+              <label><input type='radio' name='date' />Today </label>
+              <label><input type='radio' name='date' />Tomorrow </label>
+              <label><input type='radio' name='date' />This week </label>
+              <label><input type='radio' name='date' />This month </label>
+
+
+            </div>
+            <div className="price-filter">
+              <h2>Price Range</h2>
+
+              <label><input type='radio' name='price' />Rs.0 - Rs.1000</label>
+              <label><input type='radio' name='price' />Rs.1000 - Rs.5000</label>
+              <label><input type='radio' name='price' />Above 5000</label>
+
+            </div>
+            <button>Clear filters</button>
+          </div>
+          <div className="home-cards-right">
+            <div className='home--cards-row'>
+
+              <div className='home--cards'>
+                {cardData.map((item, index) => <EventCard
+                  key={index}
+                  date={item.date}
+                  month={item.month}
+                  rating={item.rating}
+                  image={item.img}
+                  title={item.Title}
+                  location={item.location}
+                />)}
+              </div>
+            </div>
+
+
+            <div className='home--cards-row'>
+
+              <div className='home--cards'>
+                {cardData.map((item, index) => <EventCard
+                  key={index}
+                  date={item.date}
+                  month={item.month}
+                  rating={item.rating}
+                  image={item.img}
+                  title={item.Title}
+                  location={item.location}
+                />)}
+              </div>
+            </div>
+
+            <div className='home--cards-row'>
+
+
+              <div className='home--cards'>
+                {cardData.map((item, index) => <EventCard
+                  key={index}
+                  date={item.date}
+                  month={item.month}
+                  rating={item.rating}
+                  image={item.img}
+                  title={item.Title}
+                  location={item.location}
+                />)}
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
-)
+  )
 }
 
 export default Search
